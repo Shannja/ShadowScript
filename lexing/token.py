86be546a0276b75b4facaaa45b21c22816df5fef
Shannja <103908889+Shannja@users.noreply.github.com>
2023-04-05 15:57:46 -0400

    Finished lexing module

--- lexing/token.py
+++ lexing/token.py
@@ -2,4 +2,11 @@ class Token:
     def __init__(self, kind, line):
         self.kind = kind
         self.line = line
-        self.string = ""
+        self.locale, self.string = line.new_locale()
+        self.string = self.string or 'End Of File'
+
+    def __repr__(self):
+        return f"({self.kind[0]})'{self.string}'"
+
+    def mark(self):
+        self.line.mark(self)
--- main.py
+++ main.py
@@ -1,12 +1,23 @@
+from lexing.lexing import Lexer
+from lexing.source_line import SourceLine
+from util.error import LanguageError
+
 def main():
     print('ShadowScript Console\nType quit to exit.')
+    lexer = Lexer()
 
     while True:
         line = input('> ')
         if line == 'quit':
             break
-            
-        print(line)
+        if line == "": continue
+
+        try:
+            line = SourceLine(line)
+            tokens = lexer.make_tokens(line)
+            print(tokens)
+        except LanguageError as error:
+            print(error)
 
 
 if __name__ == '__main__':
